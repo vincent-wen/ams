c9be37af50b33dd0ddcd4e3140b7773bb6ba7672
vincent-wen <vincent.wen77@gmail.com>
2014-11-19 10:50:24 -0500

    add sign in button of google+

--- src/main/resources/WEB-INF/js/controllers.js
+++ src/main/resources/WEB-INF/js/controllers.js
@@ -464,4 +464,20 @@ controller('StudentCtrl', ['$scope', '$http', 'userService', 'gradingSystem',
 				}).modal('show');
 			})
 		}
-	}]);
+	}]).
+
+controller('PaymentCtrl', ['$http', userService, function($http, userService){
+	$scope.signinCallback = function(authResult) {
+		if (authResult['status']['signed_in']) {
+	    // Update the app to reflect a signed in user
+	    angular.element('#signinButton').hide();
+	  } else {
+	    // Update the app to reflect a signed out user
+	    // Possible error values:
+	    //   "user_signed_out" - User is signed-out
+	    //   "access_denied" - User denied access to your app
+	    //   "immediate_failed" - Could not automatically log in the user
+	    console.log('Sign-in state: ' + authResult['error']);
+	  }
+	}
+}]);
