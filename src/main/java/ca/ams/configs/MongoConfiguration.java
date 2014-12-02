2cb20523ee43a0d829640d7e1f359ad0379ac9d4
vincent-wen <vincent.wen77@gmail.com>
2014-12-02 15:06:06 -0500

    secure mongodb

---
+++ src/main/java/ca/ams/configs/MongoConfiguration.java
@@ -0,0 +1,24 @@
+package ca.ams.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.authentication.UserCredentials;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import com.mongodb.Mongo;
+
+@Configuration
+public class MongoConfiguration {
+
+  public @Bean MongoDbFactory mongoDbFactory() throws Exception {
+    UserCredentials userCredentials = new UserCredentials("amsUser", "ams777");
+    return new SimpleMongoDbFactory(new Mongo(), "ams", userCredentials);
+  }
+
+  public @Bean MongoTemplate mongoTemplate() throws Exception {
+    return new MongoTemplate(mongoDbFactory());
+  }
+}
+
